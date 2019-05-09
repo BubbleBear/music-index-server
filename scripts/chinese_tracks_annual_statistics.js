efd6cae5b3dfb73464b952765c723a310b020f89
雪霁 <vatel@Xor.local>
2019-05-09 22:46:55 +0800

    .

---
+++ scripts/chinese_tracks_annual_statistics.js
@@ -0,0 +1,81 @@
+const fs = require('fs');
+
+const list2csv = require('./utils').list2csv;
+
+const mongo = require('mongodb');
+
+let client;
+let db;
+
+async function getDB(useClient) {
+    if (!db) {
+        client = useClient || await mongo.connect('mongodb://localhost', {
+            keepAlive: 5 * 60 * 1000,
+            useNewUrlParser: true,
+        });
+
+        db = client.db('qq_music_crawler');
+    }
+
+    return db;
+}
+
+if (require.main === module) {
+    !async function() {
+        await getDB();
+
+        const matrix = [];
+
+        const companyCursor = db.collection('company').find({}).sort({ company_id: 1 });
+
+        const yearSet = new Set;
+
+        while (await companyCursor.hasNext()) {
+            const company = await companyCursor.next();
+
+            const entry = {
+                id: company.company_id,
+                name: company.name,
+            };
+
+            const albums = await db.collection('album').find({
+                album_id: {
+                    $in: company.albumList.map(album => Number(album.album_id)),
+                },
+            }).toArray();
+
+            albums.forEach((album) => {
+                const year = album.aDate.split('-')[0];
+
+                yearSet.add(year);
+
+                entry[year] = entry[year] === undefined ? album.total : entry[year] + album.total;
+            });
+
+            matrix.push(entry);
+
+            // console.log(company.company_id);
+
+            if (company.company_id > 1000) {
+                break;
+            }
+        }
+
+        const years = [...yearSet].sort((a, b) => {
+            return Number(a) - Number(b);
+        });
+
+        const head = years.reduce((acc, cur) => {
+            acc.set(cur, cur);
+            return acc;
+        }, new Map( [['company_id', 'id'], ['name', '唱片公司']] ));
+
+        const csv = list2csv(matrix, head).replace(/"undefined"/g, '0');
+
+        const ws = fs.createWriteStream('x.csv');
+        ws.write(csv);
+        ws.end();
+
+        client.close();
+    }()
+}
---
+++ scripts/utils.js
@@ -0,0 +1,53 @@
+"use strict";
+exports.__esModule = true;
+var chinese_conv_1 = require("chinese-conv");
+function list2csv(list, map) {
+    var headerMap;
+    if (map && map instanceof Map === false) {
+        headerMap = new Map(Object.entries(map));
+    }
+    else {
+        headerMap = map;
+    }
+    var collumns = headerMap && Array.from(headerMap.keys()) || Object.keys(list[0] || {});
+    var header = collumns.map(function (v) { return "\"" + (headerMap ? headerMap.get(v) : v) + "\""; }).join(',');
+    var content = list.reduce(function (acc, cur) {
+        acc += collumns.map(function (key) { return "\"" + cur[key] + "\""; }).join(',');
+        acc += '\r\n';
+        return acc;
+    }, '');
+    return header + '\r\n' + content;
+}
+exports.list2csv = list2csv;
+function filterUndefinedAndEmpty(obj) {
+    if (Array.isArray(obj)) {
+        var r = obj.map(function (o) { return filterUndefinedAndEmpty(o); }).filter(function (o) { return o; });
+        return r.length ? r : undefined;
+    }
+    else if (obj && typeof obj === 'object') {
+        var filtered = Object.keys(obj).reduce(function (acc, key) {
+            var f = filterUndefinedAndEmpty(obj[key]);
+            return f && (acc[key] = f), acc;
+        }, {});
+        return Object.keys(filtered).length ? filtered : undefined;
+    }
+    return obj;
+}
+exports.filterUndefinedAndEmpty = filterUndefinedAndEmpty;
+function normalizeString(target) {
+    var result = chinese_conv_1.sify(target).toLowerCase().replace(/_/g, '-');
+    return result;
+}
+exports.normalizeString = normalizeString;
+if (require.main === module) {
+    var list = [
+        {
+            a: 1,
+            b: 2,
+            c: 3
+        }
+    ];
+    var map = new Map([['c', 'c'], ['a', 'a'], ['b', 'b']]);
+    var csv = list2csv(list, map);
+    console.log(csv);
+}
